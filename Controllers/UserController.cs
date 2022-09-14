4876b1add05fac77463dadbbf1d311edbb35efca
Yatendra Dalal <yat.dal@gmail.com>
2022-09-14 10:17:39 +0530

    Login page

---
+++ Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnettraining.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index(string p1, string p2)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/UserCredential.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnettraining.Models
+{
+    public class UserCredential
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Message { get; set; } = string.Empty;
+    }
+}
